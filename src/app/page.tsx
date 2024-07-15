11901baafc755e7e2c18607587d39df938fae794
Osamu Mitsuhashi <osamu.mitsuhashi224@gmail.com>
2024-07-15 13:42:06 +0900

    ✨文言の変更とレイアウトの対応

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,31 @@
-import {
-  AppBar,
-  Box, Button, TextField, Toolbar,
-  Typography,
-} from "@mui/material";
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-      <>
-        {/* header */}
+    <>
+      {/* header */}
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: "transparent", boxShadow: "none" }}
+      >
+        <Toolbar>
+          <Typography color="black" variant="h6" sx={{ flexGrow: 1 }}>
+            Logo(WIP)
+          </Typography>
+          <Button color="primary">お問い合わせ</Button>
+        </Toolbar>
+      </AppBar>
 
-        {/* body */}
-        <Box
-            display="flex"
-            flexDirection="column"
-            alignItems="center"
-            justifyContent="center"
-            minHeight="100vh"
-            textAlign="center"
-        >
+      {/* body */}
+      <Box
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+        minHeight="30vh"
+        textAlign="center"
+      >
+        <Box>
           <Typography variant="h4" gutterBottom>
             価値を創造します
           </Typography>
@@ -34,8 +42,9 @@ export default function Home() {
             妥協もありません
           </Typography>
         </Box>
+      </Box>
 
-        {/* footer */}
-      </>
+      {/* footer */}
+    </>
   );
 }
